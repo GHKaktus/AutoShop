84f0fc3fae30f858fb1ff1139703e441624f1acb
levchenkiNikitaLaptopHuawei <avantix5@gmail.com>
2026-06-10 19:28:44 +0300

    docs(frontend): добавить комментарий в точку входа main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store/store.ts'
 import './index.css'
 import App from './App.tsx'
-
+// Корневой файл
 createRoot(document.getElementById('root')!).render(
 
     <StrictMode>
